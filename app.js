9bad3bba1469f82289f947fedd6f388180019b37
renee <reneebruff@gmail.com>
2018-06-11 13:12:06 -0400

    firebase initialized

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAmEN8gXxK1i3ExANwY1IFDzmorMA-kN94",
+  authDomain: "class-projects-dfa52.firebaseapp.com",
+  databaseURL: "https://class-projects-dfa52.firebaseio.com",
+  projectId: "class-projects-dfa52",
+  storageBucket: "class-projects-dfa52.appspot.com",
+  messagingSenderId: "618644801913"
+};
+firebase.initializeApp(config);
+
